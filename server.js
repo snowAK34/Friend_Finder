fbe0f67487dd488b52e42d1a0a5df7693ecdf59f
Stacy Nowak <nowak.stacy@gmail.com>
2019-04-03 12:46:30 -0700

    some html content and path setup to test heroku deployment

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
